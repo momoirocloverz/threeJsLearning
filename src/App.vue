8bff011438f3b2ecd31e7ce984d6a36ebf9900d3
momoclo <1796583678@qq.com>
2022-11-09 17:55:18 +0800

    初次尝试

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,44 @@
+<template>
+  <div id="container"></div>
+  <canvas class="webgl"></canvas>
+</template>
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue'
+import * as THREE from 'three'
+
+onMounted(() => {
+  const scene = new THREE.Scene()
+
+  //   console.log('container', container)
+  //   container.appendChild(renderer.domElement)
+
+  const sizes = {
+    width: 800,
+    height: 600,
+  }
+
+  const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+  camera.position.z = 3
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
+  const material = new THREE.MeshBasicMaterial({
+    color: '#ff0000',
+  })
+  const mesh = new THREE.Mesh(geometry, material)
+
+  let canvas = document.querySelector('.webgl')
+
+  const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+  })
+  renderer.setSize(sizes.width, sizes.height)
+
+  scene.add(mesh)
+  scene.add(camera)
+
+  renderer.render(scene, camera)
+})
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped lang="less"></style>
